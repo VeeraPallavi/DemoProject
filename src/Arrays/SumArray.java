89c6a9a2cc02f5af5dd609a8340c7840075e8668
VeeraPallavi <pallavinandimandalam@gmail.com>
2025-12-23 12:46:15 +0530

    Added Array Programs

---
+++ src/Arrays/SumArray.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+public class SumArray
+{
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40};
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
